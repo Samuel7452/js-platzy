42d49e152f3b89b6beb39bfdd1b255d87b1f38a4
Samuel7452 <Samuellopezgarcia745@gmail.com>
2023-01-10 08:05:42 -0500

    start second class

---
+++ js intermedio/script.js
@@ -0,0 +1,15 @@
+const h1 = document.querySelector('h1');
+const p = document.querySelectorAll('p');
+const pclase = document.querySelector('.parrafo1');
+const pid = document.querySelector('#parrafo2');
+const input = document.querySelector('input');
+
+console.log(input.value);
+
+console.log({
+    h1,
+    p,
+    pclase,
+    pid,
+    input
+})
